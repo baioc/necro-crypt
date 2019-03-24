fa206052c2ee8ff317b457ba118f954877d9db14
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-03-24 06:08:01 +0000

    Add Player movement
    Created base Entity class

---
+++ entities/player/Player.gd
@@ -0,0 +1,16 @@
+extends "res://entities/Entity.gd"
+
+
+func control(delta):
+	velocity = ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += -1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+
+	velocity = velocity.normalized() * speed
